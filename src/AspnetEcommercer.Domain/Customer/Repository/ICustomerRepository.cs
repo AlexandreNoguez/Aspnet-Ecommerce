f3c8531fa332658ce5fe5978f13ac15deadd7c3d
Alexandre Noguez <Alexandre Noguez@ALEX-PC>
2025-12-15 08:42:03 -0300

    feat: add mapper and customer interface

---
+++ src/AspnetEcommercer.Domain/Customer/Repository/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using AspnetEcommercer.Domain.Customer.Entity;
+
+namespace AspnetEcommercer.Domain.Customer.Repository;
+
+public interface ICustomerRepository
+{
+    Task AddAsync(CustomerEntity customer, CancellationToken ct = default);
+    Task UpdateAsync(CustomerEntity customer, CancellationToken ct = default);
+    Task<CustomerEntity?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<IReadOnlyList<CustomerEntity>> GetAllAsync(CancellationToken ct = default);
+    Task DeleteAsync(Guid id, CancellationToken ct = default);
+}
---
+++ src/AspnetEcommercer.Infrastructure/Customer/Mappers/CustomerMapper.cs
@@ -0,0 +1,67 @@
+﻿using AspnetEcommercer.Domain.Customer.Entity;
+using AspnetEcommercer.Domain.Customer.ValueObject;
+using AspnetEcommercer.Infrastructure.Customer.Models;
+
+namespace AspnetEcommercer.Infrastructure.Customer.Mappers;
+
+public static class CustomerMapper
+{
+    public static CustomerDbModel ToDbModel(CustomerEntity entity)
+    {
+        return new CustomerDbModel
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            IsActive = entity.IsActive,
+            RewardPoints = entity.RewardPoints,
+
+            Street = entity.Address?.Street,
+            City = entity.Address?.City,
+            State = entity.Address?.State,
+            ZipCode = entity.Address?.ZipCode,
+            Number = entity.Address?.Number
+        };
+    }
+
+    public static CustomerEntity ToDomain(CustomerDbModel model)
+    {
+        Address? address = null;
+
+        // If you want to build Address only when it is complete
+        if (model.Street is not null &&
+            model.City is not null &&
+            model.State is not null &&
+            model.ZipCode is not null &&
+            model.Number is not null)
+        {
+            address = new Address(
+                model.Street,
+                model.City,
+                model.State,
+                model.ZipCode,
+                model.Number.Value
+            );
+        }
+
+        return new CustomerEntity(
+            model.Id,
+            model.Name,
+            address,
+            model.IsActive,
+            model.RewardPoints
+        );
+    }
+
+    public static void ApplyToDbModel(CustomerEntity entity, CustomerDbModel model)
+    {
+        model.Name = entity.Name;
+        model.IsActive = entity.IsActive;
+        model.RewardPoints = entity.RewardPoints;
+
+        model.Street = entity.Address?.Street;
+        model.City = entity.Address?.City;
+        model.State = entity.Address?.State;
+        model.ZipCode = entity.Address?.ZipCode;
+        model.Number = entity.Address?.Number;
+    }
+}
