e42ef6e6cd63776acd15c96a293d156f08c55a89
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-12-16 21:04:14 -0300

    feat: add infra dependencies injections and fix namespaces

--- src/AspnetEcommerce.WebApi/Extensions/DependencyInjection.cs
+++ src/AspnetEcommerce.WebApi/Extensions/DependencyInjection.cs
@@ -1,9 +1,14 @@
-﻿using AspnetEcommerce.Application.Customer.UseCases.CreateCustomer;
-using AspnetEcommercer.Domain.Contracts.Abstractions;
-using AspnetEcommercer.Domain.Customer.Repository;
-using AspnetEcommercer.Infrastructure.Customer.Repository;
-using AspnetEcommercer.Infrastructure.Database;
-using AspnetEcommercer.Infrastructure.Persistence;
+﻿using AspnetEcommerce.Application.Contracts.Email;
+using AspnetEcommerce.Application.Customer.Contracts.Email;
+using AspnetEcommerce.Application.Customer.UseCases.CreateCustomer;
+using AspnetEcommerce.Domain.Contracts.Abstractions;
+using AspnetEcommerce.Domain.Customer.Repository;
+using AspnetEcommerce.Infrastructure.Customer.Repository;
+using AspnetEcommerce.Infrastructure.Database;
+using AspnetEcommerce.Infrastructure.Email.SmtpConfig;
+using AspnetEcommerce.Infrastructure.Email.Templates.CustomerWelcome;
+using AspnetEcommerce.Infrastructure.Messaging;
+using AspnetEcommerce.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspnetEcommerce.WebApi.Extensions;
@@ -24,8 +29,19 @@ public static class DependencyInjection
         // Repository (Infra)
         services.AddScoped<ICustomerRepository, CustomerRepositoryEf>();
 
-        // Use Cases (Application)
-        //services.AddScoped<ICreateCustomerUseCase, CreateCustomerUseCase>();
+        // RabbitMQ options + fila
+        services.Configure<RabbitMqOptions>(configuration.GetSection("RabbitMq"));
+        services.AddSingleton<IEmailJobQueue, RabbitMqEmailJobQueue>();
+
+        // SMTP + template (usados pelo worker)
+        services.Configure<SmtpOptions>(configuration.GetSection("SmtpOptions"));
+        services.AddScoped<IEmailSender, SmtpEmailSender>();
+        services.AddScoped<ICustomerEmailTemplateProvider, CustomerEmailTemplateProvider>();
+
+        // Worker para consumir fila de e-mails
+        services.AddHostedService<WelcomeEmailWorker>();
+
+        // Use cases
         services.AddScoped<ICreateCustomerUseCase, CreateCustomerUseCase>();
 
         return services;
