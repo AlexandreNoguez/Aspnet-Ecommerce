36bd06687e1a729fcd88e3cbde1d50582167078b
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2026-01-15 19:49:57 -0300

    fix: category interface use case

--- src/AspnetEcommerce.Application/Product/UseCases/GetCategoryById/IGetCategoryByIdUseCase.cs
+++ src/AspnetEcommerce.Application/Product/UseCases/GetCategoryById/IGetCategoryByIdUseCase.cs
@@ -1,42 +1,8 @@
-﻿using AspnetEcommerce.Application.Product.DTOs.GetProductById;
-using AspnetEcommerce.Application.Product.Exceptions;
-using AspnetEcommerce.Domain.Product.Repository;
+﻿using AspnetEcommerce.Application.Product.DTOs.GetCategoryById;
 
-namespace AspnetEcommerce.Application.Product.UseCases.GetProductById;
+namespace AspnetEcommerce.Application.Product.UseCases.GetCategoryById;
 
-public sealed class GetProductByIdUseCase : IGetProductByIdUseCase
+public interface IGetCategoryByIdUseCase
 {
-    private readonly IProductRepository _productRepository;
-
-    public GetProductByIdUseCase(IProductRepository productRepository)
-    {
-        _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
-    }
-
-    public async Task<GetProductByIdOutput> ExecuteAsync(GetProductByIdInput input, CancellationToken ct = default)
-    {
-        if (input is null) throw new ArgumentNullException(nameof(input));
-        if (input.Id == Guid.Empty)
-        {
-            throw new ValidationException("Id is required.");
-        }
-
-        var product = await _productRepository.GetByIdAsync(input.Id, ct);
-        if (product is null)
-        {
-            throw new NotFoundException($"Product with id '{input.Id}' was not found.");
-        }
-
-        return new GetProductByIdOutput(
-            product.Id,
-            product.Name.Value,
-            product.Description.Value,
-            product.Price.Amount,
-            product.Price.Currency,
-            product.Sku.Value,
-            product.Category.Id,
-            product.Category.Name,
-            product.StockQuantity,
-            product.IsActive);
-    }
+    Task<GetCategoryByIdOutput> ExecuteAsync(GetCategoryByIdInput input, CancellationToken ct = default);
 }
